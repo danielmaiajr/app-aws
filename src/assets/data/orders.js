72333272c26b35a716000543b9f0c41744e7d405
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-06-08 14:19:46 -0300

    Added dummy orders data

---
+++ src/assets/data/orders.js
@@ -0,0 +1,16 @@
+export const orders = [
+	{
+		id: 1,
+		quantity: 13,
+		day: 25,
+		month: 'mai',
+		status: 'concluído'
+	},
+	{
+		id: 2,
+		quantity: 21,
+		day: 26,
+		month: 'mai',
+		status: 'concluído'
+	}
+];
