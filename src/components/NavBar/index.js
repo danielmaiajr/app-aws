005c076fca84b888d0666d98203ce2d38aa4688c
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-06-08 14:44:13 -0300

    Changed container style to material-ui Container component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,36 +1,43 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
 
 import Logo from './Logo';
 import Search from './Search';
 import SelectedAddress from './SelectedAddress';
 import Profile from './Profile';
-import Container from '../Container/Container';
 
 export default function NavBar() {
 	const classes = useStyles();
 
 	return (
-		<Container styles={classes.root}>
-			<Logo />
-			<Search />
-			<SelectedAddress />
-			<Profile />
-		</Container>
+		<div className={classes.root}>
+			<Container className={classes.container}>
+				<Logo />
+				<Search />
+				<SelectedAddress />
+				<Profile />
+			</Container>
+		</div>
 	);
 }
 
 const useStyles = makeStyles(() => ({
 	root: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		background: '#f7f7f7',
-		boxShadow: 'inset 0 -1px 0 #dcdcdc',
+		height: 80,
+		width: '100%',
 		position: 'fixed',
 		top: 0,
 		left: 0,
-		height: 80,
-		zIndex: 100
+		zIndex: 100,
+		background: '#f7f7f7',
+		boxShadow: 'inset 0 -1px 0 #dcdcdc'
+	},
+	container: {
+		height: '100%',
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center'
 	}
 }));
